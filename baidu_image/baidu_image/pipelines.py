487fdb965a72a7e50054c8f87bf4d76148b7dfd0
lifanxin <951860555@qq.com>
2018-08-09 10:19:19 +0800

    Add files via upload

--- baidu_image/baidu_image/pipelines.py
+++ baidu_image/baidu_image/pipelines.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
 
 # Define your item pipelines here
 #
@@ -6,6 +8,17 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class BaiduImagePipeline(object):
+class BaiduImagePipeline(ImagesPipeline):
     def process_item(self, item, spider):
+        return item
+
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url)
+            
+    def item_completed(self, results, item, info):
+        image_path = [x['path'] for ok,x in results if ok]
+        if not image_path:
+            raise scrapy.exceptions.DropItem('Item contains no images')
+        item['image_paths'] = image_path
         return item
